bf0d5b583cb519f6018e773f0deca7838d30a5df
taliSent <sentemovanatalya@yandex.ru>
2022-04-25 09:49:12 +0300

    Update App.tsx

--- src/App.tsx
+++ src/App.tsx
@@ -4,7 +4,6 @@ import Menu from "./components/menu/Menu";
 import styles from "./App.module.css";
 import Links from "./components/links/Links";
 import { ThemeContext } from "./context/context";
-import { useEffect } from "react";
 
 const App = () => {
   const { theme } = useContext(ThemeContext);
