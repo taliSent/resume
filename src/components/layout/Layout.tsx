8970802ea9940c46f4a5530810fc310ed848a0d2
taliSent <sentemovanatalya@yandex.ru>
2022-04-29 15:30:31 +0300

    z-index bug fix

--- src/components/layout/Layout.tsx
+++ src/components/layout/Layout.tsx
@@ -1,12 +1,9 @@
 import React from 'react';
-import { HashLink } from 'react-router-hash-link';
 import About from './sections/about/About';
 import ContactMe from './sections/contactMe/ContactMe';
 import Experience from './sections/education/Experience';
 import Stack from './sections/stack/Stack';
 import Testimonials from './sections/testimonials/Testimonials';
-import MediaQuery from 'react-responsive';
-import { BsArrowUpCircle } from 'react-icons/bs';
 
 const Layout: React.FC = () => {
     return (
