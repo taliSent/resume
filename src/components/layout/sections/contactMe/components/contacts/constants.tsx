f16562da2c339755ac3bbecc3bb32ea57e4312f1
taliSent <sentemovanatalya@yandex.ru>
2024-01-05 02:37:03 +0300

    phone updated

--- src/components/layout/sections/contactMe/components/contacts/constants.tsx
+++ src/components/layout/sections/contactMe/components/contacts/constants.tsx
@@ -4,26 +4,26 @@ import { MdOutlineAlternateEmail } from "react-icons/md";
 
 const MY_TG = "https://t.me/TaliSent";
 const MY_MAIL = "ns.sent@yandex.ru";
-const MY_PHONE = "+995557169890";
+const MY_PHONE = "+79688899087";
 
 const CONTACTS = [
   {
     href: MY_TG,
     textPrimary: "Text me",
     textSecondary: "TaliSent",
-    icon: <BsTelegram size='30' />,
+    icon: <BsTelegram size="30" />,
   },
   {
     href: `mailto: ${MY_MAIL}`,
     textPrimary: "Email me",
     textSecondary: "ns.sent@yandex.ru",
-    icon: <MdOutlineAlternateEmail size='30' />,
+    icon: <MdOutlineAlternateEmail size="30" />,
   },
   {
     href: `tel: ${MY_PHONE}`,
     textPrimary: "Call me",
     textSecondary: MY_PHONE,
-    icon: <FiPhone size='30' />,
+    icon: <FiPhone size="30" />,
   },
 ];
 
