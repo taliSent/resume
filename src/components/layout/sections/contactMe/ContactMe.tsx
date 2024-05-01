03ee3e3963a5729a1c55e193a419b0ccbc43ddaf
TaliSent <ns.sent@yandex.ru>
2024-05-02 00:23:44 +0300

    refactoring

--- src/components/layout/sections/contactMe/ContactMe.tsx
+++ src/components/layout/sections/contactMe/ContactMe.tsx
@@ -8,27 +8,25 @@ import { HashLink } from "react-router-hash-link";
 import styles from "./ContactMe.module.css";
 import Links from "./components/links/Links";
 
-const ContactMe = () => {
-  return (
-    <section id='contact-me' aria-label='contact me'>
-      <Header
-        name='Contact me'
-        icon={<FiMail className='icon' size='28' />}
-        textSecondary='Natalia Sentemova'
-      />
-      <Contacts />
-      <MediaQuery minWidth={849}>
-        <HashLink
-          to='#top'
-          className={styles.LinkToTheTop}
-          aria-label='to the top of the page'
-          smooth
-        >
-          <BsArrowUpCircle size='22' />
-        </HashLink>
-      </MediaQuery>
-      <Links />
-    </section>
-  );
-};
+const ContactMe = () =>
+  <section id='contact-me' aria-label='contact me'>
+    <Header
+      name='Contact me'
+      icon={<FiMail className='icon' size='28' />}
+      textSecondary='Natalia Sentemova'
+    />
+    <Contacts />
+    <MediaQuery minWidth={849}>
+      <HashLink
+        to='#top'
+        className={styles.LinkToTheTop}
+        aria-label='to the top of the page'
+        smooth
+      >
+        <BsArrowUpCircle size='22' />
+      </HashLink>
+    </MediaQuery>
+    <Links />
+  </section>
+
 export default ContactMe;
