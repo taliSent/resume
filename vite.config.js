7ff115ed430d300e19cee09e78cbb789d4ba9be1
taliSent <sentemovanatalya@yandex.ru>
2025-11-17 18:25:07 +0200

    markup fix

--- vite.config.js
+++ vite.config.js
@@ -3,6 +3,7 @@ import react from "@vitejs/plugin-react";
 import path from "path";
 
 export default defineConfig({
+  base: "resume",
   plugins: [react()],
   resolve: {
     alias: {
@@ -14,5 +15,4 @@ export default defineConfig({
   build: {
     outDir: "build",
   },
-  base: "resume"
 });
