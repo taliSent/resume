5a6903e3ab1aed78a6a276f65688599947f036eb
taliSent <sentemovanatalya@yandex.ru>
2025-11-25 19:46:26 +0200

    animation fix

--- src/components/bar/Bar.tsx
+++ src/components/bar/Bar.tsx
@@ -6,14 +6,14 @@ import {
 } from "framer-motion";
 import React, { useEffect } from "react";
 import styles from "./Bar.module.css";
-import useFirstAchievement from "src/feature/useFirstAchievements";
 
 type BarProps = {
   progress: number;
+  needAnimate?: boolean;
 };
 
 const Bar: React.FC<BarProps> = ({ progress }: BarProps) => {
-  const lengthNumber = useMotionValue(0);
+  const lengthNumber = useMotionValue(progress);
   const width = useTransform(lengthNumber, (value) => `${value * 100}%`);
   const [_, animate] = useAnimate();
 
@@ -26,12 +26,7 @@ const Bar: React.FC<BarProps> = ({ progress }: BarProps) => {
   }, [progress]);
 
   return (
-    <motion.div
-      initial={{ boxShadow: "0 0 0.5rem purple" }}
-      animate={{ boxShadow: "0 0 0 purple" }}
-      transition={{ duration: 1.3, ease: "easeInOut" }}
-      className={styles.Bar}
-    >
+    <motion.div className={styles.Bar}>
       <motion.div
         className={`${styles.ProgressLevel}`}
         style={{ width }}
