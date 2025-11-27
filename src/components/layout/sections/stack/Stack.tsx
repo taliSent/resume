b8f6e3218713017e9248044f867e23a8c296e7c5
taliSent <sentemovanatalya@yandex.ru>
2025-11-27 12:48:31 +0200

    stack scroll fixed

--- src/components/layout/sections/stack/Stack.tsx
+++ src/components/layout/sections/stack/Stack.tsx
@@ -1,12 +1,17 @@
-import React from "react";
+import React, { use } from "react";
 import { GiTechnoHeart } from "react-icons/gi";
 import Header from "../../header/Header";
 import { ReactFlow, Background, Controls } from "@xyflow/react";
 import "@xyflow/react/dist/style.css";
 import { edges, nodes } from "./constants";
-import styles from './Stack.module.css';
+import styles from "./Stack.module.css";
+import { ThemeContext } from "src/context/context";
+import { useMediaQuery } from "react-responsive";
 
 const Stack: React.FC = () => {
+  const { theme } = use(ThemeContext);
+  const isDesktop = useMediaQuery({ query: "(min-width: 1024px)" }); // Example for mobile breakpoint
+
   return (
     <section id="stack" aria-label="my stack" data-screen-width={true}>
       <Header
@@ -15,7 +20,13 @@ const Stack: React.FC = () => {
         textSecondary="technologies I use"
       />
       <div className={styles.ReactFlow}>
-        <ReactFlow nodes={nodes} edges={edges} fitView>
+        <ReactFlow
+          nodes={nodes}
+          edges={edges}
+          colorMode={theme}
+          preventScrolling={!isDesktop}
+          fitView={true}
+        >
           <Background />
           <Controls />
         </ReactFlow>
