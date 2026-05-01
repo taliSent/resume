be1c9a1ddbab3bb05db3f5364fe5db6d44dcc760
taliSent <sentemovanatalya@yandex.ru>
2026-05-01 15:53:55 +0300

    facts made more interesting

--- src/constants/constants.ts
+++ src/constants/constants.ts
@@ -5,25 +5,25 @@ export const enum Themes {
 
 // move it to sc
 export const FACTS = [
-  "I love horror movies",
+  "As a child, I watched an insane amount of Asian horror movies, I'm still attracted to that genre despite being a coward",
   "I'm almost entirely indifferent about food",
   "I've had dreadlocks for about 5 years",
-  "I don't like my first name and prefer to be called Tali",
-  "I am a cat person",
-  "I'm a younger sibling in a family with 2 kids",
+  "I don't like my first name and go by Tali",
+  "I am a 100% cat person",
+  "My family is scattered across the world, and I have relatives in different countries",
   "I don't really like to take photos of myself, so I have very few",
   "I adore house plants and know the names of many species and their varieties",
   "I play tabletop roleplay games, like D&D",
-  "I used to study German language, when I was a teen",
+  "I used to study the German language as a teen, because I loved German rock bands",
   "I'm not a user of TikTok or Instagram",
-  "I play very specific short indie video games",
+  "I play very specific short indie video games like Slay the Princess, No, I'm Not a Human, and Birth",
   "I was a Dungeon Master and received a lot of positive feedback",
   "I have a natural ability to make cartoonish voices",
   "I learned to sing, but am too shy to do it in public",
   "I used to be an actress in a horror escape room",
   'My favorite book is Eliezer S. Yudkowsky\'s "Harry Potter and methods of rationality"',
-  "I used to draw and paint, and there are still traces of that somewhere in the Internet",
-  "I used to study psychology and my favorite topic is cognitive biases",
+  "I used to draw, paint, and write short stories, and there are still traces of that somewhere in the Internet",
+  "I used to study psychology as an undergraduate. My favorite topic is cognitive biases",
   "I jumped with a parachute once",
 ];
 
